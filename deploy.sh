b1d9bb7ab2e450b2a2dc7203191f74dced2b6eb0
mike <mango@live.cn>
2019-08-20 16:06:19 +0800

    updated dependecies

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 apt -y update
 apt -y upgrade
 apt -y install git
-apt -y install build-essential scons mercurial qt5-default libpng12-dev libjpeg-dev libilmbase-dev libxerces-c-dev libboost-all-dev libopenexr-dev libglewmx-dev libxxf86vm-dev libeigen3-dev libfftw3-dev libpcrecpp0v5 libcollada-dom2.4-dp-dev
+apt -y install build-essential scons mercurial qt5-default libpng-dev libjpeg-dev libilmbase-dev libxerces-c-dev libboost-all-dev libopenexr-dev libglewmx-dev libxxf86vm-dev libeigen3-dev libfftw3-dev libpcrecpp0v5 libcollada-dom2.4-dp-dev
 pip install --upgrade pip
 pip install numpy==1.16
 pip install pyexr
