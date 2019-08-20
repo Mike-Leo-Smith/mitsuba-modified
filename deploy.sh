db96b7ceaa6940540da4d9d475b3b74159fd7d0b
mike <mango@live.cn>
2019-08-20 16:51:05 +0800

    using wget instead of git clone

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 apt -y update
 apt -y upgrade
 apt -y install git
+apt -y install unzip
 apt -y install build-essential scons mercurial qt5-default libpng-dev libjpeg-dev libilmbase-dev libxerces-c-dev libboost-all-dev libopenexr-dev libglewmx-dev libxxf86vm-dev libeigen3-dev libfftw3-dev libpcrecpp0v5 libcollada-dom2.4-dp-dev
 pip install --upgrade pip
 pip install numpy==1.16
 pip install pyexr
 while [ ! -f "mitsuba" ]; do 
-  git clone https://github.com/Mike-Leo-Smith/mitsuba-modified.git mitsuba
+  wget https://github.com/Mike-Leo-Smith/mitsuba-modified/archive/master.zip
+  unzip master.zip
+  mv mitsuba-modified-master mitsuba
+  rm mitsuba.zip
 done
 cd mitsuba && scons -j24 && source setpath.sh
