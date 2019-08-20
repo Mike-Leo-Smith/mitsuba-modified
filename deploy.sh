9326401c491847697943fb40ae983a90c43b9f11
mike <mango@live.cn>
2019-08-20 15:23:07 +0800

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+apt -y update
+apt -y upgrade
+apt -y install git
+apt -y install build-essential scons mercurial qt5-default libpng12-dev libjpeg-dev libilmbase-dev libxerces-c-dev libboost-all-dev libopenexr-dev libglewmx-dev libxxf86vm-dev libeigen3-dev libfftw3-dev libpcrecpp0v5 libcollada-dom2.4-dp-dev
+pip install --upgrade pip
+pip install numpy==1.16
+pip install pyexr
+git clone https://github.com/Mike-Leo-Smith/mitsuba-modified.git mitsuba
+cd mitsuba && scons -j24
+source setpath.sh
